746e94151356108c4a3fa4518adf2170133fdcb3
Noel Chinaza <44059080+noel-chinaza@users.noreply.github.com>
2022-10-06 03:38:39 +0100

    updated build command and added coverage files

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,7 @@ module.exports = {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "dist/server/coverage",
+  coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
